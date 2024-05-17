73275805826434e9fda8e71d988e40756725abb8
dawit2123 <dawitzewdu2123@gmail.com>
2024-05-17 11:33:54 +0300

    hacker rank problems doen

---
+++ hello.py
@@ -0,0 +1,26 @@
+#printing my hello world language in python
+x=[1,2,3,4]
+# print(2 in x, 10 not in x)
+
+description="This is another description in the python of the modules and I am trying to make it more clear and conscice in the data structure and algorith concepts"
+
+# print(description[-2:])
+# if (2<4):
+    # print('2 is less than 4')
+
+
+#KNOW WHAT IS HAPPENING IN THE DOWN CODE BELOW
+# x=['abebe', 'alemu','abebech']
+# father=['gashe', 'a father', 'b father']
+# for x in x:
+#     for father in father:
+#          print ('The result is: '+str(x),'and the father is '+father )
+
+#LAMDA FUNCITONS 
+# x=-121
+# if str(x) == str(x)[::-1]:
+#     print('true', str(x)[::-1])
+# else:
+#     print('false', str(x)[::-1])
+
+
